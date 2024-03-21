6da3d5cdd77a63d40a90b852b20700b903496e17
Leon <leondellafiore2016@gmail.com>
2024-03-21 15:51:32 -0300

    Funciones booleanas agregadas: biImplificacion, esPrimo conjuncion, disyuncion excluyente xor

--- Guia2.hs
+++ Guia2.hs
@@ -22,12 +22,7 @@ maxTres x y z
  | x >= y && x >= z = x
  | y >= z && y >= x = y 
  | otherwise = z
-
-absolute :: Int -> Int
-absolute x 
- | x>=0 = x 
- |otherwise = -x
-
+--Ejercicio 4 definir las funciones concatenar, tomar , dejar y agregar al final
 concatenar :: [a] -> [a] -> [a]
 concatenar a b = a ++ b
 
@@ -39,8 +34,30 @@ dejar :: Int -> [a] -> [a]
 dejar 0 (x:xs) = [x] ++ xs
 dejar n (x:xs) = [] ++ dejar (n-1) (xs)
 
+agregarAlFinal :: [a] -> a -> [a]
+agregarAlFinal lista elemento  = lista ++ [elemento] 
+
+--Ejercicio 5 definir funcion abs 
+absolute :: Int -> Int
+absolute x 
+ | x>=0 = x 
+ |otherwise = -x
+--Ejercicio 6 (Pendiente)
 edad :: (Num a) => (a,a,a) -> (a,a,a) -> a
 edad (d1,m1,a1) (d2,m2,a2)  = a1-a2
 
-edad' :: Int  => (a,a,a) -> (a,a,a) -> a
-edad' (d1,m1,a1) (d2,m2,a2)  = a1-a2
+--Ejercicio 7 : Definir  la funcion xor : disyunción exclusiva
+xor :: Bool -> Bool -> Bool 
+xor False x = x
+xor x False = x 
+xor True True = False
+--Ejemplo De funcion de conjuncion logica
+conjuncion :: Bool -> Bool -> Bool
+conjuncion False x = False  
+conjuncion True x = x 
+ --Ejercicio8 Defina una fucion que dado un numero natural defina si es primo o no
+esPrimo :: Int -> Bool
+esPrimo n = [x | x <- [2..n] , mod n x == 0] == [n] 
+
+biImplication :: Bool -> Bool -> Bool
+biImplication p q = p==q
