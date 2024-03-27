43384fee2911731f9b3b94696195510e5cdcc40b
Leon <leondellafiore2016@gmail.com>
2024-03-27 17:15:53 -0300

    Exercices .13 .14 finished , .15 in progress...

--- EjercicioRecomendado.hs
+++ EjercicioRecomendado.hs
@@ -1,5 +1,3 @@
-import Data.Char
-import Data.IntSet
 --Definir una funcion que toma una secuencia y decir si todos sus digitos son pares
 secuenciaPar :: [Int] -> Bool
 secuenciaPar [] = True
@@ -40,7 +38,7 @@ firstOcurrence m (x:xs)
  | otherwise = firstOcurrence m xs
 
 --Define a function that removes the first ocurrence of an integer m
-removeFirst :: Int -> [Int] -> [Int]
+removeFirst ::(Eq a ) => a -> [a] -> [a]
 removeFirst m [] = []
 removeFirst m (x:xs)  |m==x = xs | otherwise = x : removeFirst m xs 
 
@@ -51,6 +49,37 @@ mnmInt [] = error "empty list"
 mnmInt [x] = x
 mnmInt (x:xs) = min x (mnmInt xs)
 
+--Funcion que recibe una lista de enteros (desordenada) y devuelve la lista ordenada de forma creciente
+--La forma de ordenamiento es similar al algoritmo SelectionSort
 srtInts :: [Int]->[Int]
 srtInts []=[]
 srtInts xs = m : (srtInts (removeFirst (m) (xs))) where m = mnmInt xs 
+
+--Exercise 1.13 Write a function count for counting the number of ocurrences of a caracter in a string
+
+count :: Char -> [Char] -> Int
+count c [] = 0
+count c (x:xs)
+ | c == x = 1 + count c xs
+ | otherwise = count c xs
+
+--Exercise 1.14 Write a function that take a string and returns same list but all its elements are tripicated  except the firstone
+
+triplicate :: String -> String 
+triplicate [] = []
+triplicate (x:xs) = x : x : x : triplicate xs
+
+blowup :: String -> String 
+blowup (x:xs) = [x] ++ triplicate xs  
+
+--Exercise 1.15 Write a function srtString that sorts a sring in alphabetical order
+--For this we need to obtain te minimun Char in a list
+--And we modify the removeFirst's identity to a polimorfic function
+minChar ::(Ord a ) => [a]-> a 
+--minChar [] = undefined
+minChar [x] = x
+minChar (x:xs) = min x (minChar xs)
+
+srtString ::(Eq a, Ord a) => [a] -> [a]
+srtString [] = []
+srtString (x:xs)= m : (srtString (removeFirst (m) (xs))) where m = minChar xs
