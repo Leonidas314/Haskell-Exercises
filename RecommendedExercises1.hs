417bc377ad8361bec8f724d24874d846ffc9fd20
Leon <leondellafiore2016@gmail.com>
2024-03-31 19:02:15 -0300

    lista de largo de listas y suma de largo de lista de listas

--- RecommendedExercises1.hs
+++ RecommendedExercises1.hs
@@ -14,3 +14,17 @@ lpd k n
 factorization :: Int -> [Int]
 factorization 1 = []
 factorization n = lpd 2 n : factorization(n`div`(lpd 2 n))
+
+--Map And Filter
+
+--Exercise 1.20 Use map to write a function that take a list of list and returns a list of the corresponding list length
+
+listOflengthList :: [[a]] -> [Int]
+listOflengthList l = [x | x <- map length l]
+
+
+--Exercise 1.21 Use map to write a function sumLengths that take a list of list and return the sum of their lengths
+
+sumLengths :: [[a]] -> Int 
+sumLengths [] = 0
+sumLengths (x:xs) = length x + sumLengths xs
