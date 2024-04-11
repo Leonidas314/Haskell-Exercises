8786a4cb4a1838155e5f39b53b9fd0a443162bcc
Leon <leondellafiore2016@gmail.com>
2024-04-11 17:50:56 -0300

    Soluciones factoriales terminado

--- EjercicioFactorial.hs
+++ EjercicioFactorial.hs
@@ -11,21 +11,42 @@ factorialguards n
 
 --Distintas formas de crear una lista con los factoriales de n:
 
-
+--Primera solucion
 --Factorial con una definicion local de aux
 factoriales_1 :: Int -> [Int]
 factoriales_1 n =
     reverse (aux n) where aux 0 = [1] 
                           aux n = factorial n : aux(n-1)
 
-
---Definicion recursiva con uso de acumladores
+--Segunda solucion :
+--Definicion recursiva con uso de acumladores --Actividad: Completar
 factoriales_2 :: Int -> [Int]
 factoriales_2 n =
     reverse(aux(n+1) 0 [1]) --reverse esta demás con la siguiente def.
     where aux n m (x:xs) = if m == n 
             then [] else
                             if m/= n then factorial m: aux n (m+1) (x:xs)
-                             else error "Sos un FRACASADO"
+                             else error "NaN"
                              --No mentira geniooooo
-         
+
+--Tercera solucion: Por listas de comprensión
+factoriales_3 :: Int -> [Int]
+factoriales_3 n = [factorial x | x <- [0..n]]
+
+--Cuarta Solucion Definicion de orden superior
+factoriales_4 :: Int -> [Int]
+factoriales_4 n = map factorial [0..n]
+
+--Actividad proponer una nueva solucion con la funcion scanl
+{-scanl toma una funcion binaria, una semilla y una lista y devuelve los resultados
+acumulados de aplicar la funcion a la semilla y a los valores sucesivos-}
+
+-- Atencion! Las funciones definidas previamente NO son binarias 
+
+factbin :: Int -> Int -> Int
+factbin 0 _ = 1
+factbin 1 _ = 1
+factbin n _ = n* factbin (n-1) 0  
+
+factoriales_5 :: Int -> [Int]
+factoriales_5 n = scanl (*) 1 [1..n]
