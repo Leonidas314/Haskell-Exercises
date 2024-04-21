a1d257200634253edff24dee178eff8ad15af267
Leon <leondellafiore2016@gmail.com>
2024-04-21 13:02:55 -0300

    Correciones menores en Derivacion.hs y Guia.5.hs

--- Derivacion.hs
+++ Derivacion.hs
@@ -1,21 +1,20 @@
 
 --Programas calculados por derivacion
-
 ----Ejercicio18 Definir la funcion 
 --todosOcurrenEn :: Eq a => [a] -> [a] -> Bool
 --tal que todosOcurrenEn xs ys se verifica si todos los elementos de xs son elementos de ys 
 --todosOcurrenEn [1,5,2,5] [5,1,2,4] = True
 --todosOcurrenEn [1,5,2,5] [5,2,4] = False
 
-existElem :: Eq a => [a] -> a -> Bool
-existElem [] _= False
-existElem (y:ys) x = (x==y) || existElem ys x
+existElem :: Eq a => a -> [a] -> Bool
+existElem _ []= False
+existElem x (y:ys) = (x==y) || existElem x ys
 
 toEn :: Eq a => [a] -> [a] -> Bool
 toEn [] _ = True 
-toEn (x:xs) (ys) = existElem ys x && toEn xs ys 
+toEn (x:xs) (ys) = existElem x ys && toEn xs ys 
 
 
-prefix' :: String -> String -> Bool
-prefix' [] _ = True
-prefix' (x:xs) (y:ys) = x == y && prefix' xs ys
+prefijo :: String -> String -> Bool
+prefijo [] _ = True
+prefijo (x:xs) (y:ys) = x == y && prefijo xs ys
--- Guia5.hs
+++ Guia5.hs
@@ -129,3 +129,12 @@ divisoresN n = [x | x <- [1..n], mod n x==0]
 --tal que todosOcurrenEn xs ys se verifica si todos los elementos de xs son elementos de ys 
 --todosOcurrenEn [1,5,2,5] [5,1,2,4] = True
 --todosOcurrenEn [1,5,2,5] [5,2,4] = False
+
+
+existElem :: Eq a => a -> [a] -> Bool
+existElem _ []= False
+existElem x (y:ys) = (x==y) || existElem x ys
+
+toEn :: Eq a => [a] -> [a] -> Bool
+toEn [] _ = True 
+toEn (x:xs) (ys) = existElem x ys && toEn xs ys 
