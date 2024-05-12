f3a9134714c57b39393810a3937e6aa99c11d721
Leon <leondellafiore2016@gmail.com>
2024-05-12 19:06:22 -0300

    Guia8 especificaciones completada.

--- Guia8.hs
+++ Guia8.hs
@@ -2,10 +2,13 @@
 --Ejercicio 1:
 --a)
 sontodasIg :: Eq a => [a] -> Bool
+sontodasIg [] = True    
+sontodasIg [x] = True
 sontodasIg (x:xs)= all (==x) (x:xs)
 --b)
 sontodosDif :: Eq a => [a] -> Bool
 sontodosDif [] = True
+sontodosDif [x] = True
 sontodosDif (x:xs) = all (/=x) [y | y <-xs] && sontodosDif (xs)
 --c)
 sorted :: [Int] -> Bool
@@ -38,6 +41,27 @@ maxposTrue xs = maxList [n | n<-[0..(length xs)-1] , xs!!n==True] where maxList 
                                                                         maxList (x:xs) = max x (maxList xs)
 
 
+--Ejercicio3 
+
+--a)
+
+sameOddEven :: [Int] -> Bool
+sameOddEven xs = length [x | x <- xs , x`mod`2==0] == length[y | y <- xs , y /= 0 , y `mod`2 /= 0 ]
+--b)
+
+prefijo :: Eq a => [a] -> [a] -> Bool
+prefijo xs ys = any (ys==) [take i xs | i <- [0..(length xs)]]
+
+subsec1 :: Eq a => [a] -> [a] -> Bool
+subsec1 _ [] = True
+subsec1 [] ys = False
+subsec1 (x:xs) ys = prefijo (x:xs) ys || subsec1 xs ys
+
+--c)
+endsec ::Eq a => [a] -> [a] -> Bool
+endsec xs ys = any (==ys) [drop i xs | i <- [0..length xs]]
+
+
 --Definir split2:
 --La funcion split2 toma una lista de tipo a y devuelve una lisra de duplas con todas la forams de partir en dos a la lista ingresada.
 --La funcion se define con una lista de comprension que define la dupla, el primer elemento de la dupla es la funcion take que toma los i elementos de xs empezando por el 0 y hasta #xs
@@ -52,11 +76,30 @@ split2 xs = [(take i xs ,drop i xs) | i <- [0..length(xs)]]
 split3 :: [a] -> [([a],[a],[a])]
 split3 xs = [(as,bs,cs) | (as,ys)<-split2(xs) , (bs,cs)<-split2(ys)]
 
---Usando estas funciones definir :
---Una funcion que dadas dos listas xs e ys determine si ys es subsecuencia de xs
+--Usando estas funciones resolver :
 
+--Ejercicio4 
+--a)
+minsumsec :: [Int] -> Int
+minsumsec xs =minList [sum bs | (as,bs,cs)<-split3 xs] where minList [x] = x
+                                                             minList (x:xs) = min x (minList xs)
+maxigual :: [Int] -> Int
+maxigual xs = maxList [sum bs | (as,bs,cs)<-split3 xs ,sontodasIg bs] where maxList [x]= x
+                                                                            maxList (x:xs) = max x (maxList xs)
+maxdistinto :: [Int] -> Int
+maxdistinto xs  = maxList [sum bs | (as,bs,cs) <- split3 xs, sontodosDif bs] where maxList [x]=x
+                                                                                   maxList (x:xs) = max x (maxList xs)
+
+--Ejercicio 5                                                                                   
+--subsecuencia
 subsec ::Eq a => [a]-> [a] -> Bool
 subsec xs ys = any (==ys) [ bs | (as,bs,cs)<-split3 xs]
-
+--Subsecuencia final
 endingSubseg :: Eq a => [a]-> [a] -> Bool
 endingSubseg xs ys = any (==ys) [ cs | (as,bs,cs)<-split3 xs]
+
+--Subsegmento de suma maxima
+
+maxsumsec :: [Int] -> Int
+maxsumsec xs =maxList [sum bs | (as,bs,cs)<-split3 xs] where maxList [x] = x
+                                                             maxList (x:xs) = max x (maxList xs)
